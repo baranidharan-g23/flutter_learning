eb200fb47879c077cdd035f629af2cb20c4c05aa
Baranidharan G <77617148+baranidharan-g23@users.noreply.github.com>
2023-01-22 10:32:10 +0530

    Add files via upload

---
+++ rich text try/richtextsample.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            child: RichText(
+          text: TextSpan(
+              text: "Spike",
+              style: TextStyle(color: Colors.amber, fontSize: 40),
+              children: <TextSpan>[
+                TextSpan(
+                    text: ' Baranidharan',
+                    style: TextStyle(fontSize: 40, color: Colors.red))
+              ]),
+        )));
+  }
+}
